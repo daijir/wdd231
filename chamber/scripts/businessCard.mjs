7dae56505c4c1622d014b3105fd6af04fa83486b
daijir <dazhilangxianggen@gmail.com>
2025-05-26 10:30:05 +0900

    fixed errors

--- chamber/scripts/businessCard.mjs
+++ chamber/scripts/businessCard.mjs
@@ -17,16 +17,14 @@ export function createBusinessCard(member) {
   const card = document.createElement("div");
   card.className = "member-card";
   card.innerHTML = `
-    <div class="member-details">
       <div class="member-logo">
-        <img src="${member.image}" alt="${member.name} Logo" loading="lazy" width="350" height="350">
+        <img src="${member.image}" alt="${member.name} Logo" loading="lazy">
       </div>
       <strong>${member.name}</strong><br>
       <p><b>Email:</b> ${member.email}</p>
       <p><b>Phone:</b> ${member.phoneNumber}</p>
       <p><b>Address:</b> ${member.address}</p>
       <p><b>Description:</b></br> ${member.description}</p>
-    </div>
   `;
   return card;
 }
--- chamber/scripts/index.mjs
+++ chamber/scripts/index.mjs
@@ -11,8 +11,11 @@ document.addEventListener("DOMContentLoaded", async () => {
   const directory = document.getElementById("business-cards");
   try {
     const businesses = await getMembers();
-    const filtered = businesses.filter(member => member.membershipLevel === 3);
-    displayMembers(filtered, directory, createBusinessCard);
+    const filtered = businesses.filter(member => member.membershipLevel === 2 || member.membershipLevel === 3);
+    const shuffled = filtered.sort(() => Math.random() - 0.5);
+    const count = Math.floor(Math.random() * 2) + 2;
+    const randomMembers = shuffled.slice(0, count);
+    displayMembers(randomMembers, directory, createBusinessCard);
   } catch (error) {
     directory.innerHTML = "<p>Business data could not be loaded.</p>";
   }
